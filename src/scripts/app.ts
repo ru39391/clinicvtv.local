b8d0c417a270cb4d8aafed22a6dc3c044368a7d6
ru39391 <dev@39391.ru>
2026-06-04 16:57:43 +0300

    feature/создать структуру шаблона

---
+++ src/scripts/app.ts
@@ -0,0 +1,50 @@
+// @ts-ignore
+import Twig, { Template } from 'twig';
+
+const parseData = (tpl: Template): Node[] => {
+  const parser = new DOMParser();
+
+  const { body } = parser.parseFromString(
+    tpl.render(),
+    'text/html'
+  );
+
+  return Array.from(body.children);
+}
+
+const fetchTemplate = async (): Promise<Template | undefined> => {
+  try {
+    const res = await fetch('src/assets/templates/tpl.twig');
+    const data = await res.text();
+
+    return Twig.twig({ data });
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+const initApp = () => {
+  console.log('initApp');
+};
+
+const renderData = async () => {
+  const wrapper = document.querySelector<HTMLDivElement>('#app');
+
+  try {
+    const tpl = await fetchTemplate();
+    const arr = parseData(tpl as Template);
+
+    arr.forEach(item => wrapper?.append(item));
+    initApp();
+  } catch(err) {
+    console.error(err);
+  }
+};
+
+const init = () => {
+  import.meta.env.VITE_APP_ENV === 'development' ? renderData() : initApp();
+};
+
+export {
+  init
+};
