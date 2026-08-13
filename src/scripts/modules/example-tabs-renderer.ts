fa9dfd0ab63db85aac02c75d61b00cc99a375a82
ru39391 <dev@39391.ru>
2026-08-13 23:37:17 +0300

    feature/настроить отображение активного примера работ

--- src/scripts/modules/example-tabs-renderer.ts
+++ src/scripts/modules/example-tabs-renderer.ts
@@ -1,5 +1,5 @@
 import TabsRenderer, { type TExtTabsOptions } from './tabs-renderer';
-import Utils, { type TCommonData, type TResponseData } from '../utils';
+import Utils, { STATE_MOD, type TCommonData, type TResponseData } from '../utils';
 // @ts-ignore
 import { Template } from 'twig';
 
@@ -13,6 +13,19 @@ class ExampleTabsRenderer<T extends TCommonData> extends TabsRenderer<T> {
     super(options);
   }
 
+  handleRowHoverEvent(event: Event) {
+    const { id } = { id: this.tabData?.id };
+    const pane = this.panes.find(({ dataset }) => dataset.id === id);
+
+    if(!pane) return;
+
+    const target = event.target as HTMLElement;
+    const rows = Array.from(pane.querySelectorAll(this.itemSel)).filter(row => row !== target);
+
+    target.classList.add(STATE_MOD.active);
+    rows.forEach(row => row.classList.remove(STATE_MOD.active));
+  }
+
   renderFeatureItem(str: string): HTMLElement {
     const featureRow = document.createElement('p');
 
@@ -20,7 +33,7 @@ class ExampleTabsRenderer<T extends TCommonData> extends TabsRenderer<T> {
     return featureRow;
   }
 
-  renderExampleItem({ id, name, desc, introtext, img_before, img_after }: T): HTMLElement {
+  renderExampleItem({ id, name, desc, introtext, img_before, img_after }: T, index: number): HTMLElement {
     const { webp_before, thumb_before } = {
       webp_before: img_before?.webp || '',
       thumb_before: img_before?.thumb || ''
@@ -30,7 +43,7 @@ class ExampleTabsRenderer<T extends TCommonData> extends TabsRenderer<T> {
       thumb_after: img_after?.thumb || ''
     };
     const row = Utils.parseData({
-      data: { id, webp_before, thumb_before, webp_after, thumb_after },
+      data: { id, classMod: index === 0 ? " is-active" : "", webp_before, thumb_before, webp_after, thumb_after },
       tpl: this.itemRow as Template,
       rowSel: this.itemSel
     }) as HTMLElement;
@@ -59,10 +72,12 @@ class ExampleTabsRenderer<T extends TCommonData> extends TabsRenderer<T> {
 
     const itemHolder = pane?.querySelector(this.itemHolderSel);
 
-    arr.forEach(data => {
-      const row = this.renderExampleItem(data);
+    arr.forEach((data, index) => {
+      const row = this.renderExampleItem(data, index);
 
       if(!row) return;
+
+      row.addEventListener('mouseenter', this.handleRowHoverEvent.bind(this));
 
       itemHolder?.append(row);
     });
